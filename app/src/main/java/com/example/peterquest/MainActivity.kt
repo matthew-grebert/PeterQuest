12ddf33c78fbaf8eb03ac73b72c9fd236b19b03b
matthew-grebert <matthew.grebert@gmail.com>
2019-11-28 14:46:35 +0100

    first commit

---
+++ app/src/main/java/com/example/peterquest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.peterquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
